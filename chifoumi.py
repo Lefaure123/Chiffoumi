e0a91d33e828f4d79c231d0bada32ed9e82b0936
unknown <Lefaure123>
2022-07-25 11:50:29 +0200

    first step is ok

--- chifoumi.py
+++ chifoumi.py
@@ -11,10 +11,13 @@ print('** Byenvini nan jwet Chifoumi osinon woch, papye, sizo **\n')
 chans = 10
 while chans > 0:
     antre = input("tanpri chwazi youn nan opsyon sa yo W,P,S : ")
-    if str(antre) != woch and str(antre) != sizo and str(antre) != papye:
-        print("Reeseye, chwazi ant W, S, P ")
-    elif str(antre) == woch or str(antre) == sizo or str(antre) == papye:
+    if str(antre) == woch or str(antre) == sizo or str(antre) == papye:
         break
+    elif str(antre) == woch.lower() or str(antre) == sizo.lower() or str(antre) == papye.lower():
+        break
+    else:
+        if str(antre) != woch and str(antre) != sizo and str(antre) != papye:
+            print("Reeseye, chwazi ant W, S, P ")
     chans -= 1
 
 
